87adfca30a01d3965507d92a05d38b1155544184
DESKTOP-QGILQOA\grzeg <grzegorz.kuziel@gmail.com>
2024-07-26 15:04:38 +0200

    load items at startup

--- app/src/main/java/com/gkuziel/skyrssreader/ui/MainActivity.kt
+++ app/src/main/java/com/gkuziel/skyrssreader/ui/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.state.observe(this) {
             rssItemAdapter.setItems(it)
         }
+        viewModel.loadData()
     }
 
     private fun initViews() {

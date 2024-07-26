34c131ad1c4bea16829278caf267308710e98b77
DESKTOP-QGILQOA\grzeg <grzegorz.kuziel@gmail.com>
2024-07-26 03:35:47 +0200

    package structure fixed

--- app/src/main/java/com/gkuziel/skyrssreader/ui/MainActivity.kt
+++ app/src/main/java/com/gkuziel/skyrssreader/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gkuziel.skyrssreader
+package com.gkuziel.skyrssreader.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gkuziel.skyrssreader.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
